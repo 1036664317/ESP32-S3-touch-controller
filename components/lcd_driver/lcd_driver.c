8159624ac6a848b65992e63c7ae0376f9c83f2f5
ming <yiming.qin@winspread.com>
2026-09-10 15:30:21 +0800

    fix: add driver/spi_master.h and driver/gpio.h includes

--- components/lcd_driver/lcd_driver.c
+++ components/lcd_driver/lcd_driver.c
@@ -1,5 +1,7 @@
 #include "lcd_driver.h"
 #include "driver/ledc.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
